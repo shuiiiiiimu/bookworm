e38877d4aa2cf74b3ef8763cdafd992665c1335e
zhangsm <765732980@qq.com>
2014-05-26 22:58:39 +0800

    modify navigation

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -10,4 +10,16 @@ module ApplicationHelper
     end
   end
 
+  def notice_message
+    flash_messages = []
+
+    flash.each do |type, message|
+      type = :success if type.to_sym == :notice
+      text = content_tag(:div, link_to("x", "#", :class => "close", 'data-dismiss' => "alert") + message, :class => "alert alert-#{type}")
+      flash_messages << text if message
+    end
+
+    flash_messages.join("\n").html_safe
+  end
+
 end
